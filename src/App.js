5939ba59e1b5b1ff36ac337e19f9707eeb443bc7
DikshantVerma <dikshantverma411@gmail.com>
2023-09-04 22:34:46 +0530

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./App.css";
 import UtilityBar from "./UtilityBar";
-import SliderComponent from "./Slider";
-import ImageSlider from "./ImageSlider";
 import Footer from "./Footer";
 
 function App() {
   const images = ["https://unsplash.com/photos/ukzHlkoz1IE"];
   return (
     <div className="App">
-      <SliderComponent />
       <UtilityBar />
-      <ImageSlider images={images} />
       <Footer />
       {/* The rest of your app */}
     </div>
