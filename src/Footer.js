4d269f846e62a0cbc5a85e252e1eb677d700eaa2
DikshantVerma <dikshantverma411@gmail.com>
2023-09-04 23:05:32 +0530

    Separated footer columns

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,18 @@ import "./Footer.css"; // Create this file for styling
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <div className="footer">
+      <div className="footer__slogan">
+        <p>test</p>
+      </div>
+      <div className="footer__info">
+        <p>test2</p>
+      </div>
+      <div className="footer_socials">
+        <p>test3</p>
+      </div>
       <p>&copy; 2023 Apple Site. All rights reserved.</p>
-    </footer>
+    </div>
   );
 };
 
