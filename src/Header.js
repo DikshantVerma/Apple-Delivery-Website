95fef96057c592103111f1654c14d2801d7cc34a
DikshantVerma <dikshantverma411@gmail.com>
2023-09-06 00:03:00 +0530

    Added new working logo image in vs code

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Header.css";
+import logo from "./assests/pngegg.png";
 
 function Header() {
   return (
     <nav className="header">
       <img
         className="header__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/8/81/Niagara_on_tree%2C_National_Fruit_Collection_%28acc._1962-036%29.jpg"
-        alt=""
+        src={logo}
+        alt="logo"
+        width={100}
+        height={82}
+        loading="lazy"
       />
     </nav>
   );
