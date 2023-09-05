dfa863d14a9e72f8a0aa9f31e8d6f6d19cacd88a
DikshantVerma <dikshantverma411@gmail.com>
2023-09-06 01:38:50 +0530

    Installed material UI icons

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import logo from "./assests/pngegg.png";
 import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Header() {
   return (
@@ -10,21 +11,30 @@ function Header() {
         <img className="header__logo" src={logo} alt="logo" />
       </Link>
 
-      <div className="headerNav">
+      <SearchIcon className="header__searchIcon" />
+      <input type="text" className="header__searchInput" />
+
+      <div className="header__Nav">
         {/* 1st Link */}
+        <Link to="/" className="header__link">
+          <div className="header__Option">
+            <span className="header__optionlineOne">Home</span>
+          </div>
+        </Link>
+        {/* 2nd Link */}
         <Link to="/shop" className="header__link">
           <div className="header__Option">
             <span className="header__optionlineOne">Shop</span>
           </div>
         </Link>
-        {/* 2nd Link */}
+        {/* 3rd Link */}
         <Link to="/ourfarms" className="header__link">
           <div className="header__Option">
             <span className="header__optionlineOne">Our Farms</span>
           </div>
         </Link>
 
-        {/* 3rd Link */}
+        {/* 4th Link */}
         <Link to="/about" className="header__link">
           <div className="header__Option">
             <span className="header__optionlineOne">About</span>
