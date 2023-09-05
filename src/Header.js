eaf04ed81724651d34c0f714e791b62fe9cc6dab
DikshantVerma <dikshantverma411@gmail.com>
2023-09-05 23:57:17 +0530

    Added logo image

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <nav className="header">
+      <img
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/8/81/Niagara_on_tree%2C_National_Fruit_Collection_%28acc._1962-036%29.jpg"
+        alt=""
+      />
+    </nav>
+  );
 }
 
 export default Header;
--- src/OurFarms.js
+++ src/OurFarms.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./OurFarms.css";
 
 function OurFarms() {
-  return <div>OurFarms</div>;
+  return (
+    <div className="farms">
+      <h1>Welcome to Our Farms!</h1>
+    </div>
+  );
 }
 
 export default OurFarms;
