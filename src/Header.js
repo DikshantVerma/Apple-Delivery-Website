16ead317ae8821b3d6878d7110a52d1163f084a6
DikshantVerma <dikshantverma411@gmail.com>
2023-09-06 01:08:26 +0530

    Added csss styles and color coding

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,36 @@
 import React from "react";
 import "./Header.css";
 import logo from "./assests/pngegg.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="header">
-      <a href="http://localhost:3000/">
+      <Link to="/">
         <img className="header__logo" src={logo} alt="logo" />
-      </a>
+      </Link>
+
+      <div className="headerNav">
+        {/* 1st Link */}
+        <Link to="/shop" className="header__link">
+          <div className="header__Option">
+            <span className="header__optionlineOne">Shop</span>
+          </div>
+        </Link>
+        {/* 2nd Link */}
+        <Link to="/ourfarms" className="header__link">
+          <div className="header__Option">
+            <span className="header__optionlineOne">Our Farms</span>
+          </div>
+        </Link>
+
+        {/* 3rd Link */}
+        <Link to="/about" className="header__link">
+          <div className="header__Option">
+            <span className="header__optionlineOne">About</span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
